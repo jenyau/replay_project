d8b465dab892e67389a73490778ba5085157e4a2
jenyau <jekharitonov@gmail.com>
2020-10-07 00:57:07 +0300

    change file search_new.js

--- app/javascript/packs/search_new.js
+++ app/javascript/packs/search_new.js
@@ -1,14 +1,14 @@
-$(document).on('turbolinks:load', function() {
-  $(".btn").click(function(){
-    var start_station;
-    var end_station;
+// $(document).on('turbolinks:load', function() {
+//   $(".btn").click(function(){
+//     var start_station;
+//     var end_station;
 
-    start_station = $('#start_station_id');
-    end_station = $('#end_station_id');
+//     start_station = $('#start_station_id');
+//     end_station = $('#end_station_id');
 
-    if(start_station.val() == end_station.val()){
+//     if(start_station.val() == end_station.val()){
 
-      return false;
-    }
-  })
-})
+//       return false;
+//     }
+//   })
+// })
